9b6528eb2691911099a050316ae384161c0d17d3
coderalejo <jasamcyberwarrior@gmail.com>
2024-08-20 00:05:01 +0300

    Refactor e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :ExpTestImg="ExpTestImg" />
+</template>
+
+<script>
+import Vue from 'vue'
+import ExpTestImg from '@ExpTestImg/core'
+import { Dashboard } from '@ExpTestImg/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    ExpTestImg: () => new ExpTestImg(),
+  },
+}
+</script>
+
+<style src="@ExpTestImg/core/dist/style.css"></style>
+<style src="@ExpTestImg/dashboard/dist/style.css"></style>
