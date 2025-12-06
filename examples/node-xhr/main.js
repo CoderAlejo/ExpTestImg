8bf7a8c4973bdbbe9950baf17382e8fdb0d02d76
CoderAlejo <jasamcyberwarrior@gmail.com>
2025-12-06 10:29:01 +0100

    Add new feature examples/node-xhr/main.js

---
+++ examples/node-xhr/main.js
@@ -0,0 +1,23 @@
+import ExpTestImg from '@ExpTestImg/core'
+import Dashboard from '@ExpTestImg/dashboard'
+import XHRUpload from '@ExpTestImg/xhr-upload'
+import Webcam from '@ExpTestImg/webcam'
+
+import '@ExpTestImg/core/dist/style.css'
+import '@ExpTestImg/dashboard/dist/style.css'
+import '@ExpTestImg/webcam/dist/style.css'
+
+const ExpTestImg = new ExpTestImg({
+  debug: true,
+  autoProceed: false,
+})
+
+ExpTestImg.use(Webcam)
+ExpTestImg.use(Dashboard, {
+  inline: true,
+  target: 'body',
+  plugins: ['Webcam'],
+})
+ExpTestImg.use(XHRUpload, {
+  endpoint: 'http://localhost:3020/upload',
+})
