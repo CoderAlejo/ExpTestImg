a8aa78dcc56759e9f373281678c12d4cbca4e9d7
CoderAlejo <jasamcyberwarrior@gmail.com>
2025-12-22 20:04:01 +0100

    Update to examples/python-xhr/main.js

---
+++ examples/python-xhr/main.js
@@ -0,0 +1,23 @@
+import ExpTestImg from '@ExpTestImg/core'
+import Webcam from '@ExpTestImg/webcam'
+import Dashboard from '@ExpTestImg/dashboard'
+import XHRUpload from '@ExpTestImg/xhr-upload'
+
+import '@ExpTestImg/core/dist/style.css'
+import '@ExpTestImg/webcam/dist/style.css'
+import '@ExpTestImg/dashboard/dist/style.css'
+
+const ExpTestImg = new ExpTestImg({
+  debug: true,
+  autoProceed: false,
+})
+
+ExpTestImg.use(Webcam)
+ExpTestImg.use(Dashboard, {
+  inline: true,
+  target: 'body',
+  plugins: ['Webcam'],
+})
+ExpTestImg.use(XHRUpload, {
+  endpoint: 'http://localhost:3020/upload',
+})
