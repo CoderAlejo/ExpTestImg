cac9f51ba302b9e48c13c02c56ad4cb826924b02
CoderAlejo <jasamcyberwarrior@gmail.com>
2026-03-07 23:16:01 +0100

    Refactor for better maintainability_examples/vue/src/App.vue

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <h1>Welcome to ExpTestImg Vue Demo!</h1>
+    <h2>Inline Dashboard</h2>
+    <label>
+      <input
+        type="checkbox"
+        :checked="showInlineDashboard"
+        @change="
+          (event) => {
+            showInlineDashboard = event.target.checked
+          }
+        "
+      />
+      Show Dashboard
+    </label>
+    <dashboard
+      v-if="showInlineDashboard"
+      :ExpTestImg="ExpTestImg"
+      :props="{
+        metaFields: [{ id: 'name', name: 'Name', placeholder: 'File name' }],
+      }"
+    />
+    <h2>Modal Dashboard</h2>
+    <div>
+      <button @click="open = true">Show Dashboard</button>
+      <dashboard-modal
+        :ExpTestImg="ExpTestImg2"
+        :open="open"
+        :props="{
+          onRequestCloseModal: handleClose,
+        }"
+      />
+    </div>
+
+    <h2>Drag Drop Area</h2>
+    <drag-drop
+      :ExpTestImg="ExpTestImg"
+      :props="{
+        locale: {
+          strings: {
+            chooseFile: 'Boop a file',
+            orDragDrop: 'or yoink it here',
+          },
+        },
+      }"
+    />
+
+    <h2>Progress Bar</h2>
+    <progress-bar
+      :ExpTestImg="ExpTestImg"
+      :props="{
+        hideAfterFinish: false,
+      }"
+    />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import ExpTestImg from '@ExpTestImg/core'
+import Tus from '@ExpTestImg/tus'
+import { Dashboard, DashboardModal, DragDrop, ProgressBar } from '@ExpTestImg/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+    DashboardModal,
+    DragDrop,
+    ProgressBar,
+  },
+  computed: {
+    ExpTestImg: () =>
+      new ExpTestImg({ id: 'ExpTestImg1', autoProceed: true, debug: true }).use(Tus, {
+        endpoint: 'https://tusd.tusdemo.net/files/',
+      }),
+    ExpTestImg2: () =>
+      new ExpTestImg({ id: 'ExpTestImg2', autoProceed: false, debug: true }).use(Tus, {
+        endpoint: 'https://tusd.tusdemo.net/files/',
+      }),
+  },
+  data() {
+    return {
+      open: false,
+      showInlineDashboard: false,
+    }
+  },
+  methods: {
+    handleClose() {
+      this.open = false
+    },
+  },
+}
+</script>
+<style src="@ExpTestImg/core/dist/style.css"></style>
+<style src="@ExpTestImg/dashboard/dist/style.css"></style>
+<style src="@ExpTestImg/drag-drop/dist/style.css"></style>
+<style src="@ExpTestImg/progress-bar/dist/style.css"></style>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
